d8a53b8dae6438d85955b1b24101f611d8c5eeb4
wangsong <wshappyday@sina.com>
2020-04-27 23:48:40 +0800

    2020.4.27 commit

--- src/cn/wscfan/server/server/Request.java
+++ src/cn/wscfan/server/server/Request.java
@@ -17,6 +17,7 @@ public class Request {
     private String url;
     // 请求的参数
     private String queryStr;
+    private final String CRLF = "\r\n";
     public Request(Socket client) throws IOException {
         this(client.getInputStream());
     }
@@ -37,17 +38,29 @@ public class Request {
         System.out.println("----分解开始------");
         System.out.println("----1、获取请求方式-----");
         this.method = this.requestInfo.substring(0, this.requestInfo.indexOf("/")).toLowerCase();
+        this.method = this.method.trim();
         System.out.println(this.method);
         System.out.println("----2、获取url----");
         int startIdx = this.requestInfo.indexOf("/") + 1;
         int endIdx = this.requestInfo.indexOf("HTTP/");
         this.url = this.requestInfo.substring(startIdx, endIdx);
+        this.url = this.url.trim();
         int queryIdx = this.url.indexOf("?");
-        if (queryIdx > 0) {
+        if (queryIdx >= 0) {
             String[] urlArray = this.url.split("\\?");
             this.url = urlArray[0];
             this.queryStr = urlArray[1];
         }
         System.out.println(this.url);
+        if (method.equals("post")) {
+            String qStr = this.requestInfo.substring(this.requestInfo.lastIndexOf(CRLF)).trim();
+            if (null == queryStr) {
+                queryStr = qStr;
+            } else {
+                queryStr += "&" + qStr;
+            }
+        }
+        queryStr = null == queryStr ? "" : queryStr;
+        System.out.println(method + "----->" + url + "------->" + queryStr);
     }
 }
